ad17a46e5c5529274fc235c03d011c77d2a05fc2
Yuto Ichinose <yuto.0526.sano@gmail.com>
2021-12-29 00:26:21 +0900

    ビンゴカードをルーレット式で決めれるようにした

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 "use strict";
 // console.log("ok");
 
+let create_status="init";
+
 function make_hole(ele){
   console.log(ele);
   $(ele).toggleClass("hole");
 }
 
 $(function(){
-  make_card();
-  $("#make_card_btn").on("click",function () { make_card(); })
+  // make_card();
+  $("#make_card_btn").on("click",function () { 
+  
+    switch (create_status) {
+      case "init":
+        create_status="start";
+        $(this).val("ストップ");
+        let intervalId=setInterval(() => {
+          if (create_status=="stop") {
+            clearInterval(intervalId);
+          }
+          make_card();
+          
+        }, 100);
+        break;
+      case "start":
+        create_status="stop";
+        $(this).val("終了");
+        break;
+    
+      default:
+        break;
+    }
+  
+  
+  })
   // console.log($("td"));
   // console.log($("td").length);
 })
